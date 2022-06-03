90bd46d86e96a177e8755da004740e9348f97617
Jhonata Jackson <jhonata.jackson@sga.pucminas.br>
2022-06-03 09:55:06 -0300

    Implementa dados dinâmicos na tela de dashboard

---
+++ src/church-admin-front-end/src/components/dashboard/nao-batizados.js
@@ -0,0 +1,45 @@
+import { Avatar, Card, CardContent, Grid, Typography } from '@mui/material';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import {useEffect, useState} from 'react';
+
+export const NaoBatizados = function (props) {
+  console.log(">>>>>>>>>>>>> nao batizados", props.values)
+  return (
+    <Card {...props}>
+      <CardContent>
+        <Grid
+          container
+          spacing={3}
+          sx={{ justifyContent: 'space-between' }}
+        >
+          <Grid item>
+            <Typography
+              color="textSecondary"
+              gutterBottom
+              variant="overline"
+            >
+              NÃO BATIZADOS
+            </Typography>
+            <Typography
+              color="textPrimary"
+              variant="h4"
+            >
+              {`${props.values}`}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Avatar
+              sx={{
+                backgroundColor: 'primary.main',
+                height: 56,
+                width: 56
+              }}
+            >
+              <AttachMoneyIcon />
+            </Avatar>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/church-admin-front-end/src/components/dashboard/total-membros.js
@@ -0,0 +1,46 @@
+import { Avatar, Box, Card, CardContent, Grid, Typography } from '@mui/material';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import PeopleIcon from '@mui/icons-material/PeopleOutlined';
+import {useEffect, useState} from 'react';
+import axios from "axios";
+
+export const TotalMembros = function (props) {
+  return (
+    <Card {...props}>
+      <CardContent>
+        <Grid
+          container
+          spacing={3}
+          sx={{ justifyContent: 'space-between' }}
+        >
+          <Grid item>
+            <Typography
+              color="textSecondary"
+              gutterBottom
+              variant="overline"
+            >
+              TOTAL DE MEMBROS
+            </Typography>
+            <Typography
+              color="textPrimary"
+              variant="h4"
+            >
+              {`${props.values.length}`}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Avatar
+              sx={{
+                backgroundColor: 'success.main',
+                height: 56,
+                width: 56
+              }}
+            >
+              <PeopleIcon />
+            </Avatar>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+} 
